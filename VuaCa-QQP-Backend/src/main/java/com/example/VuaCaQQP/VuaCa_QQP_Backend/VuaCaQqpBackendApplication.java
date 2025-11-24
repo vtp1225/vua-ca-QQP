56e6151b6ba95d869628e5b2b6dc6af6e0d5ba99
phu <quyvo2079@gmail.com>
2025-11-24 20:35:10 +0700

    add backend

---
+++ VuaCa-QQP-Backend/src/main/java/com/example/VuaCaQQP/VuaCa_QQP_Backend/VuaCaQqpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.VuaCaQQP.VuaCa_QQP_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuaCaQqpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuaCaQqpBackendApplication.class, args);
+	}
+
+}
---
+++ VuaCa-QQP-Backend/src/test/java/com/example/VuaCaQQP/VuaCa_QQP_Backend/VuaCaQqpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VuaCaQQP.VuaCa_QQP_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuaCaQqpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
