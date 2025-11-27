e66887befae43ce94a2e81d7fdaccbeae41f083f
Ha Hong Quan <hahongquan2004@gmail.com>
2025-11-27 14:00:00 +0700

    chore(frontend): configure tailwindcss and base styles

--- fe/src/index.js
+++ fe/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+import './index.css'
+import AppRoutes from './routes/app-routes';
+import { CartProvider } from './context/cart-context';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <CartProvider>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  </CartProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
