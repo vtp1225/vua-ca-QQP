4f65d6c94335542ecfbedd3aa79877c670c56706
Nguyễn Hồng Minh Quân <nguyenhongquan20042005@gmail.com>
2025-12-16 08:00:09 +0700

    [fea]:Add new feature report in FE

---
+++ fe/src/pages/report.js
@@ -0,0 +1,22 @@
+import Header from "../components/header"
+import Footer from "../components/footer"
+import Banner from "../components/banner"
+
+export default function Home() {
+
+
+    return (
+        <div className="bg-background-light dark:bg-background-dark font-body text-text-color dark:text-gray-300 min-h-screen flex flex-col">
+            <Header />
+            <Banner />
+
+            <main className="flex-grow">
+                <div>
+                    <h1>Báo cáo</h1>
+                </div>
+            </main>
+
+            <Footer />
+        </div>
+    )
+}
